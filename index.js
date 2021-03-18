b26c2e160c069e718d85c8c44853ddc524a39e87
Jake Houston <jakechouston@gmail.com>
2021-03-18 19:12:06 -0400

    commit tree 3edf99cf817523e836463404584504a11b3fb66f

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
-
 const titleCased = () => {
-  return tutorials
+  let tutorials = [
+    'what does the this keyword mean?',
+    'What is the Constructor OO pattern?',
+    'implementing Blockchain Web API',
+    'The Test Driven Development Workflow',
+    'What is NaN and how Can we Check for it',
+    'What is the difference between stopPropagation and preventDefault?',
+    'Immutable State and Pure Functions',
+    'what is the difference between == and ===?',
+    'what is the difference between event capturing and bubbling?',
+    'what is JSONP?'
+  ];
+  return tutorials.map(function titleCaseTutorial(tutorial) {
+    let tutorialArray = [];
+    let splitTutorial = tutorial.split(" ");
+    splitTutorial.map(function (element) {
+      if (element === element.toUpperCase()) {
+        tutorialArray.push(element);
+      }
+      else {
+        tutorialArray.push(element[0].toUpperCase() + element.substring(1));
+      }
+    });
+    return tutorialArray.join(" ");
+  });
 }
